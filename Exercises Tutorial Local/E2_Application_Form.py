c68fb3990114a7ab08555547a67add9178117dfb
Sergio Andrés Soto Cano <ssotocano@gmail.com>
2020-02-14 12:05:00 -0500

    Folder for local testing

---
+++ Exercises Tutorial Local/E2_Application_Form.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome(executable_path=r"F:\Testing\Selenium_Testing\Selenium_Drivers\chromedriver\chromedriver.exe")
+driver.get("http://automationpractice.com/index.php?controller=authentication")
+
+user = driver.find_element_by_id("email")
+user.send_keys("demos@grupofasok.com")
+user.send_keys(Keys.TAB)
+
+passwd = driver.find_element_by_id("passwd")
+passwd.send_keys("Demos123*")
+passwd.send_keys(Keys.ENTER)
+
+
---
+++ Exercises Tutorial Local/E3_Unittest.py
@@ -0,0 +1,26 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+class func_unittest(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path=r"F:\Testing\Selenium_Testing\Selenium_Drivers\chromedriver\chromedriver.exe")
+
+    def test_search(self):
+        driver = self.driver
+        driver.get("http://automationpractice.com/index.php")
+        self.assertIn("My Store", driver.title)
+        element = driver.find_element_by_id("search_query_top")
+        element.send_keys("Selenium")
+        element.send_keys(Keys.RETURN)
+        time.sleep(5)
+        assert "Item not found: " not in driver.page_source
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == '__main__':
+    unittest.main()
+
